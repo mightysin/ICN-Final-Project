b6fbfd17d48a4284dc46f91e5b430bb147d01962
mightysin <94349883+mightysin@users.noreply.github.com>
2024-11-13 14:10:25 +0800

    add massage list

--- HTTPSocket/HTTPServer.py
+++ HTTPSocket/HTTPServer.py
@@ -1,17 +1,28 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
 import urllib.parse
 
+class MassageForm:
+    def __init__(self, username, time, content):
+        self.username = username
+        self.time = time
+        self.content = content
+
+messages = ['test massage1', 'test massage2', 'test massage3']
+
 class MyHandler(BaseHTTPRequestHandler):
     def do_POST(self):
+        # Keep track of all the massages from client
         content_length = int(self.headers['Content-Length'])
         post_data = self.rfile.read(content_length)
         sentence = post_data.decode('utf-8')
-        capitalized_sentence = sentence.upper()
+        messages.insert(sentence)
         
-        self.send_response(200)
-        self.send_header("Content-type", "text/plain")
-        self.end_headers()
-        self.wfile.write(capitalized_sentence.encode('utf-8'))
+        # Send back all message to client
+        for message in messages:
+            self.send_response(200)
+            self.send_header("Content-type", "text/plain")
+            self.end_headers()
+            self.wfile.write(message.encode('utf-8'))
 
 serverPort = 12000
 server = HTTPServer(('localhost', serverPort), MyHandler)
