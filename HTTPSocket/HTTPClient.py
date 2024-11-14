1b95b1e745dbecd8cd75eeb96c40a651956567c6
mightysin <chen200291@gmail.com>
2024-11-14 08:34:07 +0800

    add: chat room interface

--- HTTPSocket/HTTPClient.py
+++ HTTPSocket/HTTPClient.py
@@ -1,8 +1,20 @@
 import requests
 import tkinter as tk
+import datetime
+import time
 from tkinter import messagebox
 
-messagee = []
+messageList = []
+text_depth = 0
+
+def create_message():
+    global text_depth
+    sentence = entry.get()
+    date = datetime.date.today()
+    current_time = time.strftime("%H:%M:%S", time.localtime())
+    message = 'client' + '(' + str(date) + ' ' + current_time + ')' + ' -> ' + sentence
+    message_list.create_text(10, text_depth * 20, anchor='nw', text=message)
+    text_depth += 1
 
 def send_request():
     server_url = "https://f866-140-118-175-99.ngrok-free.app"
@@ -19,20 +31,27 @@ def send_request():
 
 # init tk window
 window = tk.Tk()
-window.title("Server Request GUI")
-window.geometry("800x600")
+window.title("Chat Room")
+window.geometry("400x600")
 
-# label, entry, button
-label = tk.Label(window, text="Input lower case:")
-label.pack(pady=5)
+# set message field
+message_field = tk.Frame(window)
+message_field.pack(side=tk.TOP, fill=tk.BOTH, expand=True)
+message_list = tk.Canvas(message_field)
+scrollbar = tk.Scrollbar(message_field, orient="vertical", command=message_list.yview)
+message_list.configure(yscrollcommand=scrollbar.set)
+message_list.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
+scrollbar.pack(side=tk.RIGHT, fill=tk.Y)
 
-entry = tk.Entry(window, width=40)
-entry.pack(pady=5)
+# set entry, button
+input_panel = tk.Frame(window)
+input_panel.pack(side=tk.BOTTOM, pady=10)
 
-send_button = tk.Button(window, text = "Send to Server", command = send_request)
-send_button.pack(pady=10)
+entry = tk.Entry(input_panel, width=35)
+entry.pack(side=tk.LEFT, anchor='w', pady=10, padx=10)
 
-result_label = tk.Label(window, text="From Server: ")
-result_label.pack(pady=5)
+# 使用 lambda 來延遲調用 create_message
+send_button = tk.Button(input_panel, text="Send to Server", command=create_message)
+send_button.pack(side=tk.RIGHT, pady=10, padx=10)
 
 window.mainloop()
